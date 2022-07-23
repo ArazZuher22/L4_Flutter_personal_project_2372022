25a3834af8caf2f6aeed10be4256c05250040744
araz z <arazzuher3@gmail.com>
2022-07-24 00:03:48 +0300

    finish color work

---
+++ mycode_wecode_project/lib/src/utils/app_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Color primary = const Color(0XFF687daf);
+
+class Styles {
+  static Color primaryColor = primary;
+  static Color textColor = const Color(0XFF3b3b3b);
+  static Color bgColor = const Color(0XFFeeedf2);
+  static Color orangeColor = const Color(0XFF526799);
+
+  static TextStyle textStyle =
+      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.bold);
+
+  static TextStyle headLineStyle1 =
+      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.bold);
+
+  static TextStyle headLineStyle2 =
+      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.bold);
+
+  static TextStyle headLineStyle3 =
+      TextStyle(fontSize: 16, color: textColor, fontWeight: FontWeight.bold);
+  
+}
