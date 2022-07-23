0e7a9e9d81bbfb719957bc42e32a740550f2f854
araz z <arazzuher3@gmail.com>
2022-07-24 00:17:09 +0300

    checking app by partition code

--- mycode_wecode_project/lib/src/screens/home_screen.dart
+++ mycode_wecode_project/lib/src/screens/home_screen.dart
@@ -1,1 +1,24 @@
 import 'package:flutter/material.dart';
+
+class HomeScreenView extends StatefulWidget {
+  const HomeScreenView({Key? key}) : super(key: key);
+  @override
+  State<HomeScreenView> createState() => _HomeScreenView();
+}
+
+class _HomeScreenView extends State<HomeScreenView> {
+  // TODO :
+  // TODO :
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text('hi'),
+        ],
+      ),
+    );
+  }
+}
